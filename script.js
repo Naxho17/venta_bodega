4fe409fa245773a54a1300dc30ee4b9d8eff0d47
Naxho17 <cristobalnaxho@gmail.com>
2025-10-04 22:18:13 -0300

    actualización de valores

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ const products = [
     images: ["images/IMG_9358.jpg"],
     description: "",
     whatsapp: "+56992925684",
-    sold: true
+    sold: false
   },
   {
     id: 7,
@@ -161,7 +161,7 @@ const videogames = [
   {
     id: 100,
     name: "Uncharted",
-    price: "$8.000",
+    price: "$5.000",
     images: ["images/Uncharted.jpg","images/Uncharted_d.jpg","images/Uncharted_a.jpg"],
     description: "Aventura de acción con exploración, tiroteos y tesoros ocultos",
     whatsapp: "+56992925684",
@@ -171,7 +171,7 @@ const videogames = [
   {
     id: 101,
     name: "Uncharted 2",
-    price: "$8.000",
+    price: "$5.000",
     images: ["images/Uncharted_2.jpg","images/Uncharted_2d.jpg","images/Uncharted_2a.jpg"],
     description: "Nathan Drake busca la legendaria ciudad Shambhala mientras entrena traiciones y enemigos mortales.",
     whatsapp: "+56992925684",
@@ -181,7 +181,7 @@ const videogames = [
   {
     id: 102,
     name: "Uncharted 3",
-    price: "$8.000",
+    price: "$5.000",
     images: ["images/Uncharted_3.jpg","images/Uncharted_3d.jpg","images/Uncharted_3a.jpg"],
     description: "Nathan Drake persigue la legendaria ciudad perdida de Ubar , enfrentando a una misteriosa organización y su propio pasado.",
     whatsapp: "+56992925684",
@@ -191,7 +191,7 @@ const videogames = [
   {
     id: 200,
     name: "Dragon Ball Raging Blast 2",
-    price: "$8.000",
+    price: "$5.000",
     images: ["images/DBZ.jpg","images/DBZ_d.jpg","images/DBZ_a.jpg"],
     description: "Juego de peleas en 3D con mas de 90 personajes de la saga Dragon ball",
     whatsapp: "+56992925684",
@@ -201,7 +201,7 @@ const videogames = [
   {
     id: 201,
     name: "GTA IV",
-    price: "$8.000",
+    price: "$5.000",
     images: ["images/Gta4.jpg","images/Gta4_d.jpg","images/Gta_4a.jpg"],
     description: "Mundo abierto Libery City con accion, historia intensa y total libertad.",
     whatsapp: "+56992925684",
@@ -211,7 +211,7 @@ const videogames = [
   {
     id: 202,
     name: "Fifa 14",
-    price: "$8.000",
+    price: "$5.000",
     images: ["images/Fifa14.jpg","images/Fifa14_d.jpg","images/Fifa14_a.jpg"],
     description: "Juego de futbol con licencias oficiales, modos variados y jugabilidad realista.",
     whatsapp: "+56992925684",
@@ -221,7 +221,7 @@ const videogames = [
   {
     id: 203,
     name: "Uncharted 4",
-    price: "$12.000",
+    price: "$10.000",
     images: ["images/Uncharted_4.jpg","images/Uncharted_4d.jpg","images/Uncharted_4a.jpg"],
     description: "Nathan Drake regresa para una ultima aventura tras la pista de un tesoro pirata, enfrentando a viejos rivales.",
     whatsapp: "+56992925684",
@@ -231,7 +231,7 @@ const videogames = [
   {
     id: 204,
     name: "Call of duty black ops 3",
-    price: "$12.000",
+    price: "$10.000",
     images: ["images/Bo3.jpg","images/Bo3_d.jpg","images/Bo3_a.jpg"],
     description: "shooter con campaña, modo multijugador y operaciones cooperativas seguidilla del Black ops 2",
     whatsapp: "+56992925684",
